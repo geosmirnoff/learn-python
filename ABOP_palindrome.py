461c98f3c1eb668ed3c47aa88e368d9a6f345e1f
George Smirnov <geosmirnoff@yandex.ru>
2019-04-12 10:39:23 +0300

    Ignore punctuation and upper case

--- ABOP_palindrome.py
+++ ABOP_palindrome.py
@@ -1,11 +1,20 @@
 def reverse(text):
     return text[::-1]
 
+def del_chars(text):
+    forbidden = (',', '.', ':', ';', '!', '?', '\\', '|', '/', '(', ')', '-', ' ', '_', '*', '"', '+', '=', '[', ']', '{', '}', '<', '>', '`', '~', '%', '$', '@', '^', '&')
+    text = text.lower()
+    for char in forbidden:
+        if char in text:
+            text = text.replace(char, '')
+    return text
+            
 def is_palindrome(text):
-    forbidden = (',', '.', ':', ';', '!', '?', '\\', '|', '/', '(', ')', '-', ' ', '_')
+    text = del_chars(text)
     return text == reverse(text)
 
 something = input('Введите текст: ')
+
 if (is_palindrome(something)):
     print('Палиндром')
 else:
